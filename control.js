fde2207acff368a4b8d1e943992d5c9bf8acf83d
AlisMilen <92162462+AlisMilen@users.noreply.github.com>
2024-02-27 21:32:26 +0100

    Add files via upload

---
+++ control.js
@@ -0,0 +1,12 @@
+document.getElementById('play').addEventListener('click',function(){
+    window.location.href='miJuego.html';
+})
+
+
+document.getElementById('instrucciones').addEventListener('click',function(){
+    window.location.href='instrucciones.html';
+})
+
+// document.getElementById('exit').addEventListener('click',function(){
+//     window.location.href='inicio.html';
+// })
